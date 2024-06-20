9d123d064074c8d44428fcb7c5bebc426a2947b2
unknown <samuel.lekpor@gmail.com>
2024-06-20 14:04:46 +0000

    Set up a sort

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import AppointmentInfo from "./components/AppointmentInfo";
 function App() {
   let [appointmentList, setAppointmentList] = useState([]);
   let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("asc")
 
   const filteredAppointments = appointmentList.filter((appointment) => {
     return (
@@ -17,6 +19,11 @@ function App() {
         .toLowerCase()
         .includes(query.toLowerCase()) ||
       appointment.aptNotes.toLowerCase().includes(query.toLowerCase())
+    );
+  }).sort((a, b) => {
+    let order = orderBy === 'asc' ? 1 : -1
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
     );
   });
 
